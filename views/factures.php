1c1f4503d6a6b9af53a53e6ab210ea68aa106249
Hugo Lesueur <hugo.lesueur.hl@gmail.com>
2024-01-11 13:21:28 +0100

    fix_css

--- views/factures.php
+++ views/factures.php
@@ -5,30 +5,36 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Afficher les factures en cours</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
-    <div class="container table-container">
+    <div class="container">
         <h1 class="mt-4 mb-4">Liste des factures en cours</h1>
-        <table>
-            <thead>
+        <table class="table table-bordered">
+            <thead class="thead-dark">
                 <tr>
                     <th>Numéro de Facture</th>
                     <th>Montant Total</th>
                     <th>Date d'échéance</th>
-                    <th>Statut</th>
+                    <th>Statuts</th>
                 </tr>
             </thead>
             <tbody>
-                <?php foreach ($factures as $facture): ?>
+                <?php if (!empty($factures)): ?>
+                    <?php foreach ($factures as $facture): ?>
+                        <tr>
+                            <td><?= $facture['numero_facture']; ?></td>
+                            <td><?= $facture['montant_total']; ?></td>
+                            <td><?= $facture['date_echeance']; ?></td>
+                            <td><?= $facture['statut']; ?></td>
+                        </tr>
+                    <?php endforeach; ?>
+                <?php else: ?>
                     <tr>
-                        <td><?= $facture['numero_facture']; ?></td>
-                        <td><?= $facture['montant_total']; ?></td>
-                        <td><?= $facture['date_echeance']; ?></td>
-                        <td><?= $facture['statut']; ?></td>
+                        <td colspan="4">Aucune facture en cours.</td>
                     </tr>
-                <?php endforeach; ?>
+                <?php endif; ?>
             </tbody>
         </table>
     </div>
